269c843ddc2958dd23623790a25def764a6d1c23
mateo <mateotomasgomez@gmail.com>
2019-06-04 13:18:15 +0200

    Adds needed rollup plugins
    rollup-plugin-json is needed by react-markdown
    
    rollup-plugin-css is needed by codemirror

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,8 @@ import babel from 'rollup-plugin-babel'
 import url from 'rollup-plugin-url'
 import filesize from 'rollup-plugin-filesize'
 import progress from 'rollup-plugin-progress'
+import postcss from 'rollup-plugin-postcss'
+import json from 'rollup-plugin-json'
 import pkg from './package.json'
 
 const production = !process.env.ROLLUP_WATCH
@@ -16,6 +18,12 @@ export default {
   ],
   external: ['react', 'react-dom', 'styled-components'],
   plugins: [
+    json({
+      compact: true,
+    }),
+    postcss({
+      extensions: ['.css'],
+    }),
     progress(),
     production && filesize(),
     url({
